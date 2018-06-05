77dbeb5e8cc417286dad93ac54d9021737f28549
1036862114 <1036862114@qq.com>
2018-06-05 17:39:30 +0800

    上传作品页增加了类型
    上传作品页增加了类型

--- app/src/main/java/com/example/shan/merchant/Activity/MerchantNewProductionActivity.java
+++ app/src/main/java/com/example/shan/merchant/Activity/MerchantNewProductionActivity.java
@@ -3,12 +3,17 @@ package com.example.shan.merchant.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.shan.merchant.R;
 
@@ -23,6 +28,7 @@ public class MerchantNewProductionActivity extends AppCompatActivity {
     private TextView WorkHead;
     private Button Commit;
     private MyClickListener myClickListener;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +36,23 @@ public class MerchantNewProductionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_merchant_new_production);
         //获取控件
         findview();
+        String[] mItems = getResources().getStringArray(R.array.hairtype);
+        //建立Adapter并且绑定数据源
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,mItems);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String str=adapterView.getItemAtPosition(i).toString();
+                Log.i("ztl","点击了"+str);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
         Back.setOnClickListener(myClickListener);
         Commit.setOnClickListener(myClickListener);
     }
@@ -43,6 +66,7 @@ public class MerchantNewProductionActivity extends AppCompatActivity {
         WorkHead = findViewById(R.id.new_production_head);
         Commit = findViewById(R.id.commit_new_production);
         myClickListener = new MyClickListener();
+        spinner = findViewById(R.id.new_production_type);
     }
     private class MyClickListener implements View.OnClickListener{
         @Override
